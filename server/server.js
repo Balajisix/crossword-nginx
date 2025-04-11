9bd1f1210fba3115908cea8510253b7b1aea30d7
Balaji <balajivs0305@gmail.com>
2025-04-11 19:54:53 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const cors = require("cors");
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const gameRoutes = require('./routes/gameRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 require('dotenv').config();
-connectDB();
+// connectDB();
 
 const app = express();
 // app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 
 app.use(
   cors({
@@ -32,7 +37,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/game', gameRoutes);
 app.use('/api/admin', adminRoutes);
 
-// module.exports = app;
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
