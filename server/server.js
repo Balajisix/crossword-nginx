dce16b0060da87cbf1b49f7a103782215b6af02b
Balaji <balajivs0305@gmail.com>
2025-04-11 20:46:48 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 // app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect("mongodb+srv://brainbric:balaji@cluster0.7x8pu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
